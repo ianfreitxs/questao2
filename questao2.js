65dd5b70ccaae85c1ae2e62917f36d909aca4209
ianfreitxs <ianfreitasdesousa@uniplaclages.edu.br>
2025-04-09 23:02:31 -0300

    Add files via upload

---
+++ questao2.js
@@ -0,0 +1,11 @@
+function mostrarVariaveis() {
+    let texto = "Olá, mundo!";
+    let inteiro = 10;
+    let decimal = 3.14;
+    let booleano = true;
+
+    alert("String: " + texto);
+    alert("Inteiro: " + inteiro);
+    alert("Float: " + decimal);
+    alert("Booleano: " + booleano);
+}
